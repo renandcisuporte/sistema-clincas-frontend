564d9ba51565295993d9ecf3034e2d668871bc79
renandcisuporte <renan@dcisuporte.com.br>
2024-10-08 10:40:08 -0300

    seutp do docker para facilidade de desenvolvimento

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    PORT: process.env.PORT,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_API_URL: process.env.NEXTAUTH_API_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
