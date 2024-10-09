7471a97475a1d46346ed957bf81a662bfdf80abf
renandcisuporte <renan@dcisuporte.com.br>
2024-10-09 14:47:10 -0300

    adiciona mascara para documentos

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function maskDocument(str: string): string {
+  let doc = str.replace(/\D/g, '')
+  if (doc.length <= 11) {
+    return doc.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})$/, '$1.$2.$3-$4')
+  }
+
+  return doc.replace(
+    /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{1,2})$/,
+    '$1.$2.$3/$4-$5'
+  )
+}
