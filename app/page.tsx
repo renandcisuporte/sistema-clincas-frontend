64343a426be67fb3f400aaad734a782e2f8dc881
renandcisuporte <renan@dcisuporte.com.br>
2024-10-18 10:28:52 -0300

    refatorar index

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Image from 'next/image'
+
+export default function Page() {
   return (
-    <div className="h-svh flex flex-col items-center justify-center px-6 md:px-0">
-      CHICUTIANER
+    <div className="h-svh flex flex-col items-center justify-center px-6 md:px-0 ">
+      <Image
+        src={'/RUBRICA-SISTEMA.png'}
+        width={305}
+        height={105}
+        quality={100}
+        className="w-full max-w-80"
+        alt=""
+      />
     </div>
   )
 }
