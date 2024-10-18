8f37883ccfb75b20e6909199c315304bd0ea938d
renandcisuporte <renan@dcisuporte.com.br>
2024-10-18 10:32:12 -0300

    remove chart da dash

--- app/(application)/(dashboard)/dashboard/page.tsx
+++ app/(application)/(dashboard)/dashboard/page.tsx
@@ -1,36 +1,3 @@
-import { Chart } from '@/app/components/chart'
-import { apiFecth } from '@/app/lib/api'
-import { authOptions } from '@/auth'
-import { getServerSession } from 'next-auth'
-
-export interface ChartsInterface {
-  fantasy: string
-  title: string
-  workHours: {
-    week: string
-    workHours: number
-  }[]
-}
-
-export default async function Page() {
-  const session = await getServerSession(authOptions)
-
-  const { data } = await apiFecth(`/clinics/charts`, {
-    accessToken: session?.accessToken,
-    next: { tags: [`clinics_charts`] }
-  })
-
-  return (
-    <div className="flex space-x-4 flex-wrap">
-      {data?.map((item: ChartsInterface) => (
-        <div className="w-1/4" key={item.fantasy}>
-          <Chart
-            fantasy={item.fantasy}
-            title={item.title}
-            workHours={item.workHours}
-          />
-        </div>
-      ))}
-    </div>
-  )
+export default function Page() {
+  return <div className="flex space-x-4 flex-wrap">Olá, Bem Vindo</div>
 }
