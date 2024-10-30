322b5067dc7bfb34485c484b09733615b3b8f263
renandcisuporte <renan@dcisuporte.com.br>
2024-10-30 08:30:28 -0300

    ajustar bug de exclusão/melhorias no layout

--- app/(application)/(dashboard)/operational-capacity/_components/average-time.tsx
+++ app/(application)/(dashboard)/operational-capacity/_components/average-time.tsx
@@ -27,7 +27,7 @@ export async function AverageTime() {
       <CardContent className="flex justify-between p-0">
         <div className="flex w-full flex-col px-3 py-2 text-center even:border-l data-[active=true]:bg-muted/50 sm:px-4 sm:py-3">
           <span className="font-extrabold text-7xl w-full">
-            {data?.averageService}
+            {data?.averageService} <small>min</small>
           </span>
         </div>
       </CardContent>
